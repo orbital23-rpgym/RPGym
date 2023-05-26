42fec9ebc6c7edee5dfb1d1cf88a309920965725
Xenos F <git@xenosfio.com>
2023-05-26 20:12:56 +0800

    chore: set up eslint and prettier

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 // Optionally import the services that you want to use
 import { getAuth } from "firebase/auth";
@@ -9,15 +9,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyB0u8F4wXttc6CETFnG1pKNBzt18W-i05k",
-    authDomain: "rpgym-ddbd4.firebaseapp.com",
-    projectId: "rpgym-ddbd4",
-    storageBucket: "rpgym-ddbd4.appspot.com",
-    messagingSenderId: "181083233006",
-    appId: "1:181083233006:web:fbd1b0cb441690e7d5b1ff",
-    measurementId: "G-YGVTWRTXG6"
-  };
-  
+  apiKey: "AIzaSyB0u8F4wXttc6CETFnG1pKNBzt18W-i05k",
+  authDomain: "rpgym-ddbd4.firebaseapp.com",
+  projectId: "rpgym-ddbd4",
+  storageBucket: "rpgym-ddbd4.appspot.com",
+  messagingSenderId: "181083233006",
+  appId: "1:181083233006:web:fbd1b0cb441690e7d5b1ff",
+  measurementId: "G-YGVTWRTXG6",
+};
+
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
@@ -25,7 +25,6 @@ const auth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
 
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
