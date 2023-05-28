42e5976671afd7bbcd00caa660b62e8bc875e6c6
Xenos F <git@xenosfio.com>
2023-05-29 06:30:56 +0800

    feat: create profile page UI

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,33 +1,63 @@
 import { StyleSheet } from "react-native";
-import { Text, View } from "../../components/Themed";
+import { Screen, Card, Text, View } from "../../components/Themed";
+import { useThemeColor } from "../../components/Themed";
 
 export default function ProfileScreen() {
+  const styles = StyleSheet.create({
+    screenStyle: {
+      padding: 30,
+      paddingTop: 100,
+      gap: 10,
+    },
+    profileInfoContainer: {
+      alignItems: "center",
+      justifyContent: "flex-start",
+      flexDirection: "row",
+      gap: 20,
+      margin: 10,
+    },
+    avatarContainer: {
+      width: 120,
+      height: 120,
+      backgroundColor: useThemeColor({}, "green"),
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    profileDetailsContainer: {
+      alignItems: "flex-start",
+      justifyContent: "center",
+      flexDirection: "column",
+      flex: 1,
+      gap: 3,
+    },
+    displayNameText: {
+      fontFamily: "Header",
+      fontSize: 25,
+    },
+    text: {
+      fontFamily: "BodyRegular",
+      fontSize: 16,
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Text>(placeholder)</Text>
-    </View>
+    <Screen style={styles.screenStyle}>
+      <View style={styles.profileInfoContainer}>
+        <View style={styles.avatarContainer}>
+          <Text>AVATAR</Text>
+        </View>
+        <View style={styles.profileDetailsContainer}>
+          <Text style={styles.displayNameText}>Jim Bro</Text>
+          <Text style={styles.text}>Health: 0</Text>
+        </View>
+      </View>
+      <Card title="⚔️ Weekly Quest" headerColor={useThemeColor({}, "orange")}>
+        <Text>PLACEHOLDER</Text>
+      </Card>
+      <Card title="💪 Campaign" headerColor={useThemeColor({}, "blue")}>
+        <Text>PLACEHOLDER</Text>
+      </Card>
+    </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
