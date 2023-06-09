d5236b2b9d5ec25894ceae8fd6e175db3a6632e7
Xenos F <git@xenosfio.com>
2023-06-09 22:26:12 +0800

    feat: add quest class

--- src/rpg/quest/Quest.ts
+++ src/rpg/quest/Quest.ts
@@ -1,4 +1,27 @@
 /**
  * In-game quest.
  */
-export default class Quest {}
+export default class Quest {
+  name: string;
+  description: string;
+  maxProgress: number;
+  currentProgress: number;
+  startDateTime: Date;
+  endDateTime: Date;
+
+  constructor(
+    name: string,
+    description: string,
+    maxProgress: number,
+    currentProgress: number,
+    startDateTime: Date,
+    endDateTime: Date,
+  ) {
+    this.name = name;
+    this.description = description;
+    this.maxProgress = maxProgress;
+    this.currentProgress = currentProgress;
+    this.startDateTime = startDateTime;
+    this.endDateTime = endDateTime;
+  }
+}
