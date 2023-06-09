213c63938d619fe1981e0bf250f7077b8720198b
Xenos F <git@xenosfio.com>
2023-06-09 22:32:03 +0800

    feat: add campaign and party models

--- src/rpg/campaign/CampaignTemplate.ts
+++ src/rpg/campaign/CampaignTemplate.ts
@@ -1,4 +1,22 @@
 /**
- * Campaign template.
+ * Campaign template. (boss fight)
  */
-export default class CampaignTemplate {}
+export default class CampaignTemplate {
+  bossName: string;
+  bossImage: string;
+  description: string;
+  bossMaxHealth: number;
+  difficulty = 1;
+
+  constructor(
+    bossName: string,
+    bossImage: string,
+    description: string,
+    bossMaxHealth: number,
+  ) {
+    this.bossName = bossName;
+    this.bossImage = bossImage;
+    this.description = description;
+    this.bossMaxHealth = bossMaxHealth;
+  }
+}
