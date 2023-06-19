96e595769f0bfa1133fc8a5a96e01b31ee823707
Xenos F <git@xenosfio.com>
2023-06-19 08:33:14 +0800

    feat: add press color for button

--- library/components/Button.tsx
+++ library/components/Button.tsx
@@ -1,4 +1,10 @@
-import { Pressable, PressableProps, StyleSheet } from "react-native";
+import {
+  Pressable,
+  PressableProps,
+  PressableStateCallbackType,
+  StyleSheet,
+  ViewStyle,
+} from "react-native";
 
 import { ThemeProps, useThemeColor } from "./Themed";
 
@@ -7,12 +13,22 @@ import { dropShadow } from "constants/styles";
 
 export type ButtonProps = {
   color?: string;
+  pressedColor?: string;
   variant?: "primary" | "secondary" | "save" | "destructive" | "default";
 } & ThemeProps &
   PressableProps;
 
 export function Button(props: ButtonProps) {
-  const { color, variant, lightColor, darkColor, style, ...otherProps } = props;
+  const {
+    color,
+    variant,
+    lightColor,
+    darkColor,
+    style,
+    pressedColor,
+    disabled: isDisabled,
+    ...otherProps
+  } = props;
 
   let variantColor: keyof typeof themes.light & keyof typeof themes.dark;
   switch (variant ?? "default") {
@@ -45,13 +61,36 @@ export function Button(props: ButtonProps) {
       backgroundColor:
         color ??
         useThemeColor({ light: lightColor, dark: darkColor }, variantColor),
+      opacity: isDisabled ? 0.5 : 1,
+    },
+    pressed: {
+      backgroundColor:
+        pressedColor ?? useThemeColor({}, "buttonDefaultPressed"),
     },
   });
 
-  return (
-    <Pressable
-      style={StyleSheet.flatten([styles.button, style])}
-      {...otherProps}
-    />
-  );
+  let newStyle: (state: PressableStateCallbackType) => ViewStyle;
+  if (style) {
+    if (typeof style === "function") {
+      // Merge with given pressed style
+      newStyle = (p: PressableStateCallbackType) =>
+        p.pressed
+          ? StyleSheet.flatten([styles.button, styles.pressed, style(p)])
+          : StyleSheet.flatten([styles.button, style(p)]);
+    } else {
+      // Merge default with given style; use default pressed style
+      newStyle = (p: PressableStateCallbackType) =>
+        p.pressed
+          ? StyleSheet.flatten([styles.button, style, styles.pressed])
+          : StyleSheet.flatten([styles.button, style]);
+    }
+  } else {
+    // Use default style
+    newStyle = (p: PressableStateCallbackType) =>
+      p.pressed
+        ? StyleSheet.flatten([styles.button, styles.pressed])
+        : styles.button;
+  }
+
+  return <Pressable style={newStyle} disabled={isDisabled} {...otherProps} />;
 }
