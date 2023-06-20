a2a265083f513086a1a65fa445181c6fceff4a38
Xenos F <git@xenosfio.com>
2023-06-21 00:12:35 +0800

    fix: make hook for user context

--- library/context/UserContext.tsx
+++ library/context/UserContext.tsx
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 import { User } from "src/user/User";
 
 export const UserContext = createContext<User | undefined>(undefined);
+
+/** Hook to use UserContext. Guarantees user is not undefined. */
+export const useUserContext = () => {
+  const user = useContext(UserContext);
+  if (!user)
+    throw new Error(
+      "No UserContext.Provider found when calling useGridItemContext.",
+    );
+  return user;
+};
