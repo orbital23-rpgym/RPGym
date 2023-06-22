515e6d279265777d0236c0804c3d34638eb71b90
Clay Lim <107010728+ethanoled@users.noreply.github.com>
2023-06-23 05:28:39 +0800

    feat: implement tracking screen without stats

--- src/fitness-tracker/workout/TrackingScreen.tsx
+++ src/fitness-tracker/workout/TrackingScreen.tsx
@@ -1,14 +1,47 @@
 import { Stack } from "expo-router";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
-import PlaceholderText from "library/components/Placeholder";
-import { Screen } from "library/components/Themed";
+import TrackingOverviewCard from "./TrackingOverviewCard";
+import Workout from "./Workout";
+import WorkoutOverviewCard from "./WorkoutOverviewCard";
+
+import { AddNewLink } from "library/components/AddNewLink";
+import { Card } from "library/components/Card";
+import { Screen, Text } from "library/components/Themed";
+import { ColorSchemeContext } from "library/context/ColorSchemeContext";
+import { useUserContext } from "library/context/UserContext";
 
 export default function TrackingScreen() {
+  const colorScheme = useContext(ColorSchemeContext);
+  const user = useUserContext();
+  const styles = StyleSheet.create({
+    noWorkoutText: { textAlign: "center" },
+  });
+  const [lastWorkout, setLastWorkout] = useState<Workout | undefined>(
+    undefined,
+  );
+  useEffect(() => {
+    user.fitnessTracker.mostRecentWorkout().then(setLastWorkout);
+  }, []);
   return (
-    <Screen>
+    <Screen gap={20}>
       <Stack.Screen options={{ title: "Tracking" }} />
-      <PlaceholderText />
+      <TrackingOverviewCard />
+      {lastWorkout ? (
+        <WorkoutOverviewCard workout={lastWorkout} />
+      ) : (
+        <Card title="🏋️ Last Workout">
+          <Text style={styles.noWorkoutText}>
+            {"You haven't completed any workouts yet."}
+          </Text>
+          <AddNewLink
+            href="(tabs)/workout"
+            text="Start a new workout"
+            replace
+          />
+        </Card>
+      )}
     </Screen>
   );
 }
