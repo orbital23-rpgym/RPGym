789302829c7772e8655e8eeaa45fa1102f44334f
Clay Lim <107010728+ethanoled@users.noreply.github.com>
2023-06-23 15:49:19 +0800

    feat: add better datetime display, show custom title

--- src/fitness-tracker/workout/WorkoutOverviewCard.tsx
+++ src/fitness-tracker/workout/WorkoutOverviewCard.tsx
@@ -1,5 +1,7 @@
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import { intlFormat } from "date-fns";
 import { useContext } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import Workout from "./Workout";
 
@@ -19,27 +21,71 @@ export default function WorkoutOverviewCard(props: WorkoutOverviewCardProps) {
   const styles = StyleSheet.create({
     container: {
       width: "100%",
+      gap: 2,
     },
-    date: {
+    dateTimeContainer: {
+      flexDirection: "row",
+      rowGap: 2,
+      columnGap: 10,
+      flexWrap: "wrap",
+      alignContent: "center",
+    },
+    dateTimeText: {
       color: themes[colorScheme].textBlue,
+    },
+    dateTimeColumn: {
+      flexDirection: "row",
+      gap: 5,
+      alignItems: "center",
+      justifyContent: "center",
     },
     exerciseList: {
       width: "100%",
     },
   });
   return (
-    <Card
-      style={StyleSheet.compose(styles.container, style)}
-      title={title}
-      {...otherProps}
-    >
-      <Text style={styles.date}>
-        Completed: {workout.endDateTime.toLocaleString()}
-      </Text>
+    <Card style={style} title={title} {...otherProps}>
+      <View style={styles.container}>
+        <View style={styles.dateTimeContainer}>
+          <View style={styles.dateTimeColumn}>
+            <FontAwesome5
+              name="calendar-day"
+              size={15}
+              color={themes[colorScheme].textBlue}
+            />
+            <Text style={styles.dateTimeText}>
+              {intlFormat(workout.startDateTime, {
+                weekday: "short",
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              })}
+            </Text>
+          </View>
+          <View style={styles.dateTimeColumn}>
+            <FontAwesome
+              name="clock-o"
+              size={15}
+              color={themes[colorScheme].textBlue}
+            />
+            <Text style={styles.dateTimeText}>
+              {intlFormat(workout.startDateTime, {
+                hour: "numeric",
+                minute: "numeric",
+              })}
+              {" - "}
+              {intlFormat(workout.endDateTime, {
+                hour: "numeric",
+                minute: "numeric",
+              })}
+            </Text>
+          </View>
+        </View>
 
-      <Text numberOfLines={5} style={styles.exerciseList}>
-        {workout.exerciseNames()}
-      </Text>
+        <Text numberOfLines={5} style={styles.exerciseList}>
+          {workout.exerciseNames()}
+        </Text>
+      </View>
     </Card>
   );
 }
