c97d07d3043bac877107b77457e828b4a9a4326b
Xenos F <git@xenosfio.com>
2023-07-22 01:43:06 +0800

    fix: make header font system and add docs for temp loading screen

--- app/(auth)/loading.tsx
+++ app/(auth)/loading.tsx
@@ -1,5 +1,23 @@
+import { Stack } from "expo-router";
 import { Text } from "react-native";
 
+/**
+ * Temporary placeholder/loading screen using no external fonts.
+ *
+ * Supposed to load in lieu of default screen while fonts are loading/splash screen is visible,
+ * but doesn't seem to work regardless :/
+ */
 export default function TempLoadingScreen() {
-  return <Text>loading...</Text>;
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTitleStyle: {
+            fontFamily: "System",
+          },
+        }}
+      />
+      <Text>loading...</Text>
+    </>
+  );
 }
