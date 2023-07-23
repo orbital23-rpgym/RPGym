eb91a666f649fbfbbae6dbaad4a85c6e348a4273
Xenos F <git@xenosfio.com>
2023-07-23 08:09:22 +0800

    refactor: remove unused imports

--- src/fitness-tracker/workout/CreateWorkoutScreen.tsx
+++ src/fitness-tracker/workout/CreateWorkoutScreen.tsx
@@ -1,8 +1,3 @@
-import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
-import { Link, Stack } from "expo-router";
-import { useContext } from "react";
-import { Pressable } from "react-native";
-
 import CreateWorkoutController from "./CreateWorkoutController";
 
 import { Screen } from "library/components/Themed";
