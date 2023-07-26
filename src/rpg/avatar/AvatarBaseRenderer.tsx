b8ad7ab6208bb13b176a5a80f9d0496dc851fc82
Xenos F <git@xenosfio.com>
2023-07-26 13:32:52 +0800

    feat: update avatar renderer to support equipment

--- src/rpg/avatar/AvatarBaseRenderer.tsx
+++ src/rpg/avatar/AvatarBaseRenderer.tsx
@@ -1,9 +1,6 @@
-import { StyleSheet } from "react-native";
-
 import { AvatarLayer, avatarLayerStyles } from "./AvatarLayer";
 
 import { AVATAR_BASE_ONBODY } from "constants/avatar-base";
-import { palette } from "constants/colors";
 import { View } from "library/components/Themed";
 import AvatarBase from "src/rpg/avatar/AvatarBase";
 
--- src/rpg/avatar/AvatarEquipmentRenderer.tsx
+++ src/rpg/avatar/AvatarEquipmentRenderer.tsx
@@ -1,8 +1,6 @@
-import { StyleSheet } from "react-native";
+import { AvatarLayer, avatarLayerStyles } from "./AvatarLayer";
 
-import { avatarLayerStyles } from "./AvatarLayer";
-
-import { palette } from "constants/colors";
+import { AVATAR_EQUIP_ONBODY } from "constants/avatar-equip";
 import { View, ViewProps } from "library/components/Themed";
 import AvatarEquipment from "src/rpg/avatar/AvatarEquipment";
 
@@ -15,12 +13,48 @@ export default function AvatarEquipmentRenderer(props: AvatarEquipmentProps) {
 
   return (
     <View style={avatarLayerStyles.layer}>
-      {/* <AvatarLayer
+      <AvatarLayer
         source={
-          AVATAR_BASE.faceHair[avatarBase.hairColor][avatarBase.facialHair]
+          AVATAR_EQUIP_ONBODY[avatarEquipment.helmet.itemType ?? "helmet"][
+            avatarEquipment.helmet.material ?? "none"
+          ]
         }
-        zIndex={zIndex + 4}
-      /> */}
+      />
+      <AvatarLayer
+        source={
+          AVATAR_EQUIP_ONBODY[
+            avatarEquipment.chestplate.itemType ?? "chestplate"
+          ][avatarEquipment.chestplate.material ?? "none"]
+        }
+      />
+      <AvatarLayer
+        source={
+          AVATAR_EQUIP_ONBODY[avatarEquipment.leggings.itemType ?? "leggings"][
+            avatarEquipment.leggings.material ?? "none"
+          ]
+        }
+      />
+      <AvatarLayer
+        source={
+          AVATAR_EQUIP_ONBODY[avatarEquipment.boots.itemType ?? "boots"][
+            avatarEquipment.boots.material ?? "none"
+          ]
+        }
+      />
+      <AvatarLayer
+        source={
+          AVATAR_EQUIP_ONBODY[
+            avatarEquipment.mainHandItem.itemType ?? "mainHand"
+          ][avatarEquipment.mainHandItem.material ?? "none"]
+        }
+      />
+      <AvatarLayer
+        source={
+          AVATAR_EQUIP_ONBODY[
+            avatarEquipment.offHandItem.itemType ?? "offHand"
+          ][avatarEquipment.offHandItem.material ?? "none"]
+        }
+      />
     </View>
   );
 }
